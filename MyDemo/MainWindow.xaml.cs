88f79c13f62e1ae1568fa52ef8a7b5180d68ab76
wengzepeng <wengzepeng@pcitech.com>
2020-06-22 09:42:09 +0800

    wzp  MyDemo init

---
+++ MyDemo/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyDemo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var name = (sender as ContentControl).Content.ToString();
+            var content = (UserControl)Activator.CreateInstance(Type.GetType($"MyDemo.{name}"));
+            controlContainer.Content = content;
+        }
+    }
+}
---
+++ MyDemo/VisualCanvas.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace MyDemo
+{
+    public class VisualCanvas : Canvas
+    {
+        private List<Visual> Visuals { get; set; } = new List<Visual>();
+
+        protected override int VisualChildrenCount => Visuals.Count;
+
+        protected override Visual GetVisualChild(int index)
+        {
+            return Visuals[index];
+        }
+
+        public void AddVisual(Visual visual)
+        {
+            Visuals.Add(visual);
+
+            base.AddVisualChild(visual);
+            base.AddLogicalChild(visual);
+        }
+
+        public void RemoveVisual(Visual visual)
+        {
+            Visuals.Remove(visual);
+
+            base.RemoveVisualChild(visual);
+            base.RemoveLogicalChild(visual);
+        }
+    }
+}
