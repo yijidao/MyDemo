b364f01cc53eb874151d5815db49d3f6f9c84c41
wengzepeng <547202221@qq.com>
2020-08-06 23:22:04 +0800

    wzp  提交JS第四章笔记

--- MyJS/MySyntax.js
+++ MyJS/MySyntax.js
@@ -4,9 +4,9 @@ function test1() {
   v2 = "v2"; // 全局变量
 }
 
-test1();
-test2("123");
-test2("123","22");
+// test1();
+// test2("123");
+// test2("123","22");
 // alert(v2);
 // alert(v1);
 // alert(typeof null);
@@ -21,18 +21,36 @@ test2("123","22");
 // alert(Number.parseInt("1b"));
 // alert(Number("1b"));
 // alert( NaN > 'A');
-function test2(p1, p2) {
-  // alert(arguments[0])
-  // alert(p1)
-  // p1 = "77"
-  // alert(arguments[0])
-  // alert(p1)
-  // p2 = "88"
-  // alert(arguments[1])
-  // alert(p2)
-  alert("两个参数");
-}
-function test2(p2){
-  alert("一个参数");
-}
+// function test2(p1, p2) {
+//   // alert(arguments[0])
+//   // alert(p1)
+//   // p1 = "77"
+//   // alert(arguments[0])
+//   // alert(p1)
+//   // p2 = "88"
+//   // alert(arguments[1])
+//   // alert(p2)
+//   alert("两个参数");
+// }
+// function test2(p2){
+//   alert("一个参数");
+// }
 
+
+// function f1(p1){
+//   p1 = new Object()
+//   p1.value = "2";
+// }
+// var o1 = new Object();
+// o1.value = "1";
+// f1(o1);
+// alert(o1.value);
+
+
+function f1(){
+  with(location){
+    var t1 = "11";
+  }
+  alert(t1);
+}
+f1();
