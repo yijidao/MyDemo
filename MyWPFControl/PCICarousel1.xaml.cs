49ec1821be065d7ce2214f5f1b8203d9ca18b0a4
wengzepeng <wengzepeng@pcitech.com>
2020-08-12 15:38:06 +0800

    wzp  提交轮播控件

--- MyWPFControl/PCICarousel1.xaml.cs
+++ MyWPFControl/PCICarousel1.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -21,43 +22,59 @@ namespace MyWPFControl
     /// PCICarousel1.xaml 的交互逻辑
     /// </summary>
     [DefaultProperty("Items")]
+    [ContentProperty("Items")]
     public partial class PCICarousel1 : UserControl
     {
         public PCICarousel1()
         {
             InitializeComponent();
-            init();
 
-            void init()
+            Items = new ObservableCollection<FrameworkElement>();
+            Items.CollectionChanged += (s, e) =>
             {
-                foreach (var item in Items)
+                if (e.NewItems?.Count > 0)
                 {
-                    ContentPanel.Children.Add(item);
-                }
-                CurrentItem = Items.FirstOrDefault();
-                PageIndex = 0;
+                    foreach (var item in e.NewItems)
+                    {
 
-            }
+                        var panel = new Grid { Width = this.Width };
+                        panel.Children.Add((FrameworkElement)item);
+                        ContentPanel.Children.Add(panel);
+
+                        var pageButton = new RadioButton();
+                        pageButton.Click += (s, e) =>
+                        {
+                            var index = radioPanel.Children.IndexOf(e.OriginalSource as RadioButton);
+                            PageIndex = index;
+
+                        };
+                        radioPanel.Children.Add(pageButton);
+                    }
+                    PageIndex = 0;
+                }
+            };
         }
+
+
+
+        //public ObservableCollection<FrameworkElement> Source
+        //{
+        //    get { return (ObservableCollection<FrameworkElement>)GetValue(SourceProperty); }
+        //    set { SetValue(SourceProperty, value); }
+        //}
+
+        //// Using a DependencyProperty as the backing store for Source.  This enables animation, styling, binding, etc...
+        //public static readonly DependencyProperty SourceProperty =
+        //    DependencyProperty.Register("Source", typeof(ObservableCollection<FrameworkElement>), typeof(ownerclass), new PropertyMetadata(0));
+
+
+
+
 
         public IEnumerable<RadioButton> RadioButtonList { get; set; }
 
-        public ObservableCollection<FrameworkElement> Items
-        {
-            get { return (ObservableCollection<FrameworkElement>)GetValue(ItemsProperty); }
-            set { SetValue(ItemsProperty, value); }
-        }
 
-        // Using a DependencyProperty as the backing store for Items.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty ItemsProperty =
-            DependencyProperty.Register("Items", typeof(ObservableCollection<FrameworkElement>), typeof(PCICarousel1), new PropertyMetadata(new ObservableCollection<FrameworkElement>
-            {
-                new Rectangle{ Width=400, Fill= new SolidColorBrush( Colors.Yellow) },
-                new Rectangle{ Width=400, Fill= new SolidColorBrush( Colors.Blue) },
-                new Rectangle{ Width=400, Fill= new SolidColorBrush( Colors.Green) },
-                new Rectangle{ Width=400, Fill= new SolidColorBrush( Colors.Red) },
-            }));
-
+        public ObservableCollection<FrameworkElement> Items { get; private set; }
 
 
         public FrameworkElement CurrentItem
@@ -68,9 +85,7 @@ namespace MyWPFControl
 
         // Using a DependencyProperty as the backing store for CurrentItem.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty CurrentItemProperty =
-            DependencyProperty.Register("CurrentItem", typeof(FrameworkElement), typeof(PCICarousel1), new PropertyMetadata(null));
-
-
+            DependencyProperty.Register("CurrentItem", typeof(FrameworkElement), typeof(PCICarousel1), new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
 
 
         private int _PageIndex = -1;
@@ -81,47 +96,40 @@ namespace MyWPFControl
             set
             {
                 _PageIndex = value;
-
-                (radioPanel.Children[value] as RadioButton).IsChecked = true;
-
-                
-                ContentPanel.Margin = new Thickness(-Items.Take(value).Sum(x => x.DesiredSize.Width), 0, 0, 0);
+                if (radioPanel?.Children.Count > value)
+                {
+                    (radioPanel.Children[value] as RadioButton).IsChecked = true;
+                }
+                ContentPanel.Margin = new Thickness(-ContentPanel.Children.Cast<Grid>().Take(value).Sum(x => x.DesiredSize.Width), 0, 0, 0);
+                if (Items?.Count > value)
+                {
+                    CurrentItem = Items[value];
+                }
             }
         }
-
 
         private void Border_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (PageIndex == 0)
             {
                 PageIndex = Items.Count - 1;
-                //ContentPanel.Margin = new Thickness(-Items.Sum(x => x.DesiredSize.Width) + Items.FirstOrDefault().DesiredSize.Width, 0, 0, 0);
             }
             else
             {
                 PageIndex--;
-                //ContentPanel.Margin = new Thickness(ContentPanel.Margin.Left + CurrentItem.DesiredSize.Width, 0, 0, 0);
             }
-            CurrentItem = Items[PageIndex];
         }
 
         private void Border_MouseDown_1(object sender, MouseButtonEventArgs e)
         {
-
-
-
             if (PageIndex == Items.Count - 1)
             {
                 PageIndex = 0;
-                //ContentPanel.Margin = new Thickness(0, 0, 0, 0);
             }
             else
             {
                 PageIndex++;
-                //ContentPanel.Margin = new Thickness(ContentPanel.Margin.Left - CurrentItem.DesiredSize.Width, 0, 0, 0);
             }
-            CurrentItem = Items[PageIndex];
-
         }
 
         private void ButtonPanel_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
