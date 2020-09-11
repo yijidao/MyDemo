7a242ef31414125de7507fadef8a01d2f72b355a
wengzepeng <wengzepeng@pcitech.com>
2020-09-11 17:56:22 +0800

    wzp  提交EFCore demo

---
+++ MyEFCoreDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyEFCoreDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
