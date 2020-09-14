5129880cc1c0aecac5916c1ae8b1cbc5d6e4ae6b
wengzepeng <wengzepeng@pcitech.com>
2020-09-14 13:55:15 +0800

    wzp  EFCore demo

--- MyEFCoreDemo/Program.cs
+++ MyEFCoreDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace MyEFCoreDemo
 {
@@ -6,7 +7,28 @@ namespace MyEFCoreDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using var db = new BloggingContext();
+
+            Console.WriteLine("新增");
+            db.Add(new Blog {Url = "http://www.baidu.com"});
+            db.SaveChanges();
+
+            Console.WriteLine("查询");
+            var blog = db.Blogs.OrderBy(b => b.BlogId).First();
+
+            Console.WriteLine("更新");
+            blog.Url = "http://www.google.com";
+            blog.Posts.Add(
+                new  Post
+                {
+                    Title = "Hello World",
+                    Content = "EfCore 练习"
+                });
+            db.SaveChanges();
+
+            Console.WriteLine("删除");
+            db.Remove(blog);
+            db.SaveChanges();
         }
     }
 }
