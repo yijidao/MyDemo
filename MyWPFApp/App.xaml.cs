44d8ff5ccc46831025577ca5cbec4d9ae08cf6ae
wengzepeng <wengzepeng@pcitech.com>
2020-11-03 17:55:24 +0800

    提交异步窗体demo

--- MyWPFApp/App.xaml.cs
+++ MyWPFApp/App.xaml.cs
@@ -5,13 +5,21 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using MyWPFApp.Views;
+using Prism.Ioc;
+using Prism.Unity;
 
 namespace MyWPFApp
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+
+        protected override Window CreateShell() => Container.Resolve<MainWindow>();
     }
 }
---
+++ MyWPFApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Prism.Commands;
+using Prism.Mvvm;
+
+namespace MyWPFApp.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+
+        public string ButtonContent { get; set; } = "命令按钮";
+
+        public ICommand ClickCommand => new DelegateCommandAsync(this, async () =>
+        {
+            await Task.Delay(5000);
+            Count++;
+
+            T1(async () =>
+            {
+                await Task.Delay(100);
+                for (int i = 0; i < 10; i++)
+                {
+                    LoadedCommand.Execute(null);
+                }
+            });
+        });
+
+        private int _count;
+        public int Count
+        {
+            get { return _count; }
+            set { SetProperty(ref _count, value); }
+        }
+
+
+        public ICommand LoadedCommand => new DelegateCommandAsync(this, async () =>
+        {
+            await Task.Delay(3000);
+            Count++;
+
+        });
+
+        private void T1(Action f)
+        {
+
+        }
+
+        private void T1(Func<Task> f)
+        {
+
+        }
+    }
+}
