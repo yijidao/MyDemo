bedba3b20e72835c05a524ca1f6fdfa6723c3fc4
wengzepeng <wengzepeng@pcitech.com>
2020-11-05 17:02:30 +0800

    增加 unity demo

---
+++ MyUnityDemo/ICar.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyUnityDemo
+{
+    public interface ICar
+    {
+
+        int Run();
+    }
+
+    public class BMW : ICar
+    {
+
+        private int _kilometer; // 公里
+
+        public int Run() => ++_kilometer;
+    }
+
+    public class Ford : ICar
+    {
+        private int _kilometer;
+        public int Run() => ++_kilometer;
+    }
+
+    public class Audi : ICar
+    {
+        private int _kilometer;
+        public int Run() => ++_kilometer;
+    }
+
+    public class Driver
+    {
+        private readonly ICar _car;
+
+        public Driver(ICar car)
+        {
+            _car = car;
+        }
+
+        public void RunCar() => Console.WriteLine($"Running {_car.GetType().Name} - {_car.Run()} kilometer ");
+    }
+}
---
+++ MyUnityDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyUnityDemo
+{
+    /// <summary>
+    /// UnityContainer 的demo
+    /// 参考文档：https://www.tutorialsteacher.com/ioc/register-and-resolve-in-unity-container
+    /// https://docs.microsoft.com/en-us/previous-versions/msp-n-p/dn178462(v=pandp.30)#without-the-event-broker-container-extension
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
