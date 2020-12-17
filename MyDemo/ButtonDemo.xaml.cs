277eb8fa0149bfe40461d25ac6efa2ba5cb65219
wengzepeng <wengzepeng@pcitech.com>
2020-12-17 14:03:03 +0800

    提交按钮

--- MyDemo/ButtonDemo.xaml.cs
+++ MyDemo/ButtonDemo.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,44 @@ namespace MyDemo
         public ButtonDemo()
         {
             InitializeComponent();
+        }
+    }
+
+    public class Thickness2ThicknessConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is Thickness t) || !(parameter is string p)) return value;
+
+            var p2 = p.Trim().ToLower();
+            var result = new Thickness();
+            if (p2.Contains("l"))
+            {
+                result.Left = t.Left;
+            }
+            else if (p2.Contains("r"))
+            {
+                result.Right = t.Right;
+            }
+            else if (p2.Contains("t"))
+            {
+                result.Top = t.Top;
+            }
+            else if (p2.Contains("b"))
+            {
+                result.Bottom = t.Bottom;
+            }
+            else
+            {
+                result = t;
+            }
+            return result;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
         }
     }
 }
