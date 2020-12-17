05907e171bc7cbd8681e13616512fc597f502a02
wengzepeng <wengzepeng@pcitech.com>
2020-12-17 18:02:57 +0800

    TextBoxDemo

---
+++ MyDemo/TextBoxDemo.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyDemo
+{
+    /// <summary>
+    /// TextBoxDemo.xaml 的交互逻辑
+    /// </summary>
+    public partial class TextBoxDemo : UserControl
+    {
+        public TextBoxDemo()
+        {
+            InitializeComponent();
+        }
+    }
+
+
+    public class TextBoxWatermarkConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values?.Length != 2 || !(values[0] is string text) || !(values[1] is bool isFocused) || !string.IsNullOrEmpty(text) || isFocused) return Visibility.Collapsed;
+            return Visibility.Visible;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
