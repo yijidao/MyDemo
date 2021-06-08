2746d5be2bd264b9811293b964124347b9c6f6eb
wengzepeng <547202221@qq.com>
2021-06-08 14:30:43 +0800

    Lazy 和 ThreadLocal

--- DesignPattern/DesignPattern/SingletonPattern.cs
+++ DesignPattern/DesignPattern/SingletonPattern.cs
@@ -27,7 +27,7 @@ namespace DesignPattern.DesignPattern
 
     /// <summary>
     /// 单例饱汉模式
-    /// 其实我觉得直接饿汉就好了
+    /// 其实我觉得直接饿汉就好了，只有这个实例不常用，或者初始化非常耗资源耗时（如从数据库中取数），才考虑这种方式。
     /// </summary>
     class SingletonPattern2
     {
@@ -58,6 +58,23 @@ namespace DesignPattern.DesignPattern
         public static void DoSomething() { }
     }
 
+    /// <summary>
+    /// Lazy 关键字的单例
+    /// Lazy 和 ThreadLocal 可以配合了解。
+    /// Lazy 经常用来实现懒加载属性， ThreadLocal 用来实现线程私有变量
+    /// </summary>
+    class SingletonPattern3
+    {
+        private static readonly Lazy<SingletonPattern3> Singleton = new Lazy<SingletonPattern3>(LazyThreadSafetyMode.ExecutionAndPublication);
 
+        private SingletonPattern3()
+        {
+        }
+
+        public static SingletonPattern3 GetSingleton()
+        {
+            return Singleton.Value;
+        }
+    }
 
 }
