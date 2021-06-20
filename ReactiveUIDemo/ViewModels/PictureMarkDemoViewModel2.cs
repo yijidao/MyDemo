d0aa9bae43cb9a250915dc0a0dae28a87156f929
wengzepeng <547202221@qq.com>
2021-06-20 23:15:47 +0800

    标注

---
+++ ReactiveUIDemo/ViewModels/PictureMarkDemoViewModel2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReactiveUI;
+
+namespace ReactiveUIDemo.ViewModels
+{
+    public class PictureMarkDemoViewModel2 : ReactiveObject
+    {
+        private string myField;
+        public string MyProperty
+        {
+            get => myField;
+            set => this.RaiseAndSetIfChanged(ref myField, value);
+        }
+
+
+    }
+}
