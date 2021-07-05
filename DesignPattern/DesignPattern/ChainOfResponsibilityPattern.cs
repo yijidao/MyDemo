a2a37c797db017402e0d65188d4c2f2d8334a3cc
wengzepeng <wengzepeng@pcitech.com>
2021-07-05 17:37:43 +0800

    ResponsibilityChain

---
+++ DesignPattern/DesignPattern/ChainOfResponsibilityPattern.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.DesignPattern
+{
+    /// <summary>
+    /// 责任链模式
+    /// Avoid coupling the sender of a request to its receiver by giving more than one object a chance to handle the request.
+    /// Chain the receiving objects and pass the request along the chain until an object handles it.
+    /// 使多个对象有机会去处理请求，从而避免耦合请求的发送者和接收者。
+    /// 将多个接收者对象连成链条，并沿着这条链条去处理请求，直到有对象处理了该请求。
+    /// </summary>
+    class ChainOfResponsibilityPattern
+    {
+
+    }
+}
