7fbdf01c4f5c9ca90fe23be9a49a277f7d2bd987
wengzepeng <wengzepeng@pcitech.com>
2021-07-06 11:01:23 +0800

    ResponsibilityChain

--- DesignPattern/DesignPattern/ChainOfResponsibilityPattern.cs
+++ DesignPattern/DesignPattern/ChainOfResponsibilityPattern.cs
@@ -13,6 +13,13 @@ namespace DesignPattern.DesignPattern
     /// 使多个对象有机会去处理请求，从而避免耦合请求的发送者和接收者。
     /// 将多个接收者对象连成链条，并沿着这条链条去处理请求，直到有对象处理了该请求。
     ///
+    /// 责任链模式最重要的角色是抽象处理器角色：
+    /// 1. 定义请求的处理方法，负责判断是在当前处理器处理请求还是将请求传递到下一个处理器。HandlerMessage
+    /// 2. 定义设置链条中下一个处理器的方法。 SetNextHandler
+    /// 3. 定义自己处理响应的级别。 SetLevel
+    /// 4. 定义业务逻辑方法。 Echo
+    ///
+    /// 
     /// 优点：
     /// 1. 将请求和响应分离开。
     /// 2. 符合最小知识原则。
@@ -63,8 +70,16 @@ namespace DesignPattern.DesignPattern
 
     abstract class Handler
     {
+        /// <summary>
+        /// 设置链条中的下一个处理器。
+        /// </summary>
         public Handler NextHanlder { get; set; }
 
+        /// <summary>
+        /// 负责判断是在当前处理器处理请求还是将请求传递到下一个处理器。
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
         public Response HandleMessage(Request request)
         {
             // 请求属于当前 level 则进行处理，不属于则到下一个处理器进行处理。
@@ -73,12 +88,16 @@ namespace DesignPattern.DesignPattern
         }
 
         /// <summary>
-        /// 具体处理逻辑
+        /// 具体处理逻辑。
         /// </summary>
         /// <param name="request"></param>
         /// <returns></returns>
         protected abstract Response Echo(Request request);
 
+        /// <summary>
+        /// 获得当前处理器能处理的请求级别。
+        /// </summary>
+        /// <returns></returns>
         public abstract Level GetLevel();
 
     }
