0b9cb0f5ed22cb836b8df22c603383b471e3e87d
wengzepeng <wengzepeng@pcitech.com>
2021-10-27 15:33:01 +0800

    little demo

---
+++ DemoResource/Class1.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace DemoResource
+{
+    public class Class1
+    {
+    }
+}
---
+++ WebView2Demo/AutoRefreshDemo.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WebView2Demo
+{
+    /// <summary>
+    /// AutoRefreshDemo.xaml 的交互逻辑
+    /// </summary>
+    public partial class AutoRefreshDemo : UserControl
+    {
+        public AutoRefreshDemo()
+        {
+            InitializeComponent();
+            input.Text = "https://www.baidu.com";
+            webView2.Source = new Uri(input.Text);
+            refresh.Click += (sender, args) =>
+            {
+                var url = input.Text;
+                if (string.IsNullOrWhiteSpace(url))
+                {
+                    webView2.CoreWebView2.ExecuteScriptAsync($"alert(`url is null`)");
+                    return;
+                }
+
+                if (!url.StartsWith("http://") && !url.StartsWith("https://"))
+                {
+                    url = $"https://{url}";
+                    input.Text = url;
+                }
+
+                webView2.CoreWebView2.Navigate(url);
+            };
+
+            var o = Observable.FromEventPattern(isAuto, nameof(isAuto.Checked))
+                .Select(_ => isAuto.IsChecked == true);
+
+
+            Observable.Interval(TimeSpan.FromSeconds(120))
+                .SkipUntil(o.Where(x => x))
+                .TakeUntil(o.Where(x => !x))
+                .Repeat()
+                .ObserveOnDispatcher()
+                .Subscribe(_ =>
+                {
+                    refresh.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
+                });
+
+            isAuto.IsChecked = true;
+
+        }
+    }
+}
