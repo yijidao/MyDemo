3e23d3a6b9a1c65cb725b4b1b732e08e04c532fe
yijidao <547202221@qq.com>
2022-02-21 18:26:38 +0800

    refit demo

---
+++ PrismDemo2/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Prism;
+using Prism.DryIoc;
+using Prism.Ioc;
+
+namespace PrismDemo2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+
+        protected override Window CreateShell() => new MainWindow();
+    }
+}
---
+++ PrismDemo2/ViewModels/RefitDemo1ViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+using Refit;
+
+namespace PrismDemo2.ViewModels
+{
+    public class RefitDemo1ViewModel : BindableBase
+    {
+        public RefitDemo1ViewModel()
+        {
+            //SystemTextJsonContentSerializer
+            //RestService.For<>()
+
+
+
+
+        }
+    }
+}
